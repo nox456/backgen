dffe275fe32b0a9c18339d46fd7c4de4843a0508
Nox <gabrismar333@gmail.com>
2023-11-05 22:54:13 -0400

    "Help with commands descriptions, Prompt style 3"
    Update: "src/args/COMMANDS.js" - Added 'description' property to 'init' command;
    
    Update: "src/args/INFO.js" - Imported 'HELP_COMMANDS';
    
    Update: "src/index.js" - Used 'execute' method of command;
    
    Update: "src/lib/styles.js" - Created style3;
    
    Add: "src/templates/helpCommands.js" - Script created to show available commands and their descriptions;

--- src/args/INFO.js
+++ src/args/INFO.js
@@ -1,4 +1,5 @@
 import chalk from "chalk";
+import HELP_COMMANDS from "../templates/helpCommands.js"
 
 const style1 = chalk.blue.bold
 const style2 = chalk.green.bold
@@ -6,6 +7,6 @@ const style2 = chalk.green.bold
 export default {
     "-v": `${style2("v")}${style1("0.0.1")}`,
     "--version": `${style2("v")}${style1("0.0.1")}}`,
-    "-h": `${style2("BackGen")} ${style1("is a CLI to build backend apps structure")}`,
-    "--help": `${style2("BackGen")} ${style1("is a CLI to build backend apps structure")}`
+    "-h": `${style2("BackGen")} ${style1("is a CLI to build backend apps structure")}\n${HELP_COMMANDS}`,
+    "--help": `${style2("BackGen")} ${style1("is a CLI to build backend apps structure")}\n${HELP_COMMANDS}`
 }
